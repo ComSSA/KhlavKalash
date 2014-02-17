2837d1f111a3f0027a5c48e5ce009b4ec3ed717c
Kye Russell <me@kyerussell.com>
2014-02-17 17:22:04 +0800

    added bitcoin exchange plugin

---
+++ plugins/bitstamp.py
@@ -0,0 +1,14 @@
+from plugins.categories import IRegularCommand
+
+import bitstamp.client
+
+class Bitstamp (IRegularCommand):
+	def __init__(this):
+		this.client = bitstamp.client.Public()
+
+	def command_btc(this, args):
+		t = this.client.ticker()
+
+		# This looks absolutely disgusting
+		return "Bitstamp: \x02Last:\x0F %s \x02Bid:\x0F %s \x02High:\x0F %s \x02Low:\x0F %s \x02Ask:\x0F %s." \
+		% (t['last'], t['bid'], t['high'], t['low'], t['ask'])
