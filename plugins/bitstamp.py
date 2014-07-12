6b948b2950f72ec307864de59faf3d451fa6ffe0
Brad Schoone <brad.schoone@gmail.com>
2014-07-12 11:20:53 +0800

    Added to Bitstamp
    Now you can add an argument to ,btc and get a value for that number of
    coins.

--- plugins/bitstamp.py
+++ plugins/bitstamp.py
@@ -8,7 +8,12 @@ class Bitstamp (IRegularCommand):
 
 	def command_btc(this, user, channel, args):
 		t = this.client.ticker()
-
-		# This looks absolutely disgusting
-		return "Bitstamp: \x02Last:\x0F %s \x02Bid:\x0F %s \x02High:\x0F %s \x02Low:\x0F %s \x02Ask:\x0F %s." \
-		% (t['last'], t['bid'], t['high'], t['low'], t['ask'])
+		if len(args) == 0:
+	   		returnval = "Bitstamp: \x02Last:\x0F %s \x02Bid:\x0F %s \x02High:\x0F %s \x02Low:\x0F %s \x02Ask:\x0F %s." \
+			% (t['last'], t['bid'], t['high'], t['low'], t['ask'])
+		elif len(args)  == 1:
+			price = str(float(t['ask']) * float(args[0]))
+			returnval = "%s bitcoins is worth approx %s" \
+			% (args[0], price)
+		
+		return returnval
