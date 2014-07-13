7e831303041d3f256636f2b164043f41d7652d44
jasongi <jasongiancono@gmail.com>
2014-07-14 01:49:18 +0800

    remove debug

--- plugins/mysterybox.py
+++ plugins/mysterybox.py
@@ -56,7 +56,6 @@ class TheMysteryBox (IRegularCommand):
         if (len(self.playerlist.players) < 2):
             return 'less than 2 people registered!'
         else:
-            return 'debug'
             self.playing == True
             self.box = random.randrange(8,14)
             self.admin = admin
