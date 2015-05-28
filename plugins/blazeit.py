07d7606104393589c33d442e59d7bc7b8eb35884
Delan Azabani <delan@azabani.com>
2015-05-28 23:31:11 +0800

    Refactor blazeit.py to implement 13:13:13 detection

--- plugins/blazeit.py
+++ plugins/blazeit.py
@@ -9,6 +9,12 @@ zones = pytz.all_timezones[:]
 class BlazeIt (IRegularCommand):
 	
 	def command_blazeit(this, context, user, channel, args):
+		return this.blazeit(4, 20, both=True)
+	
+	def command_thirteen(this, context, user, channel, args):
+		return this.blazeit(13, 13, 13, both=False)
+	
+	def blazeit(this, h, m, s=0, both=False):
 		random.shuffle(zones)
 		realnow = datetime.now(pytz.timezone('Etc/UTC'))
 		bestid = None
@@ -21,33 +27,38 @@ class BlazeIt (IRegularCommand):
 				now.year,
 				now.month,
 				now.day,
-				4, 20
+				h, m, s
 			))
-			afternoon = False
+			afternoon = (h >= 11)
 			while not then > now:
-				delta = timedelta(1)
-				if afternoon:
+				whole_day = timedelta(1)
+				if both or afternoon:
 					then = tz.localize(datetime(
 						then.year,
 						then.month,
 						then.day,
-						4, 20
-					)) + delta
+						h, m, s
+					)) + whole_day
+					afternoon = (h >= 11)
 				else:
 					then = tz.localize(datetime(
 						then.year,
 						then.month,
 						then.day,
-						16, 20
+						h + 12, m, s
 					))
-				afternoon = not afternoon
+					afternoon = False
 			if then - realnow <= min:
 				bestid = tzid
 				bestthen = then
 				min = then - realnow
 		wait = (bestthen - realnow).total_seconds()
 		(minutes, seconds) = divmod(wait, 60)
-		return ('The next 4:20 is in ' + bestid + ' (UTC' +
-			bestthen.strftime('%z') +
-			'), ' + str(int(minutes)) + ' minutes and ' +
-			str(int(seconds)) + ' seconds from now')
+		if s > 0:
+			timestr = '%d:%02d:%02d' % (h, m, s)
+		else:
+			timestr = '%d:%02d' % (h, m)
+		return 'The next %s is %d:%02d away, in %s (UTC%s)' % (
+			timestr, minutes, seconds,
+			bestid, bestthen.strftime('%z')
+		)
