ComSSA/KhlavKalash

commit fdf8ffc1ec342b0ceffcc5185a77f333a64490e5

ignore JustCommit redundant titles

plugins/urlgrabber.py
@@ -14,12 +14,14 @@ class URLGrabber (ISilentCommand):
     triggers = {r'.*(http[s]?://[A-Za-z0-9&?%._~!/=+-]+).*': "url"}
 
     def trigger_url(self, context, user, channel, match):
+        if user == 'JustCommit':
+            return
         try:
             url = match.group(1)
             response = requests.get(url)
         except (requests.exceptions.ConnectionError) as e:
             print "Failed to load URL: %s" % url
-	    print "Message: %s" % e
+        print "Message: %s" % e
         else:
             soup = BeautifulSoup(response.text)
              

commit 6e9094b206763371fb7964156595407afa915a0b

Fix a silly indentation bug that I introduced in fdf8ffc1

plugins/urlgrabber.py
@@ -21,7 +21,7 @@ class URLGrabber (ISilentCommand):
             response = requests.get(url)
         except (requests.exceptions.ConnectionError) as e:
             print "Failed to load URL: %s" % url
-        print "Message: %s" % e
+            print "Message: %s" % e
         else:
             soup = BeautifulSoup(response.text)
              